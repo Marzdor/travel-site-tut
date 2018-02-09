c09d0633256977b008277bd5e52fa3c15fb6b296
Marzdor <marzdor@gmail.com>
2018-02-08 19:57:30 -0800

    Finished lesson 43 | added mobile menu js.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-var $ = require("jquery");
-import Person from "./modules/Person";
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " Now Pays Taxes")
-  }
-}
+import MobileMenu from "./modules/MobileMenu";
 
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Doe", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
